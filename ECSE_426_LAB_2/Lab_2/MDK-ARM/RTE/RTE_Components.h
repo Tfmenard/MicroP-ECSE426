cbd83b867d1df4b07f3359213b8409e2332b7878
Félix Poulin-Bélanger <felix.poulin-belanger@mail.mcgill.ca>
2017-10-19 17:16:17 -0400

    Added Lab 2

---
+++ ECSE_426_LAB_2/Lab_2/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab_2' 
+ * Target:  'Lab_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
